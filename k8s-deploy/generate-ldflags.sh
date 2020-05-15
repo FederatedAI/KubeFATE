d35e12795fc2b17062803b4ee1328962a70b8767
Layne Peng <playne@vmware.com>
2020-05-16 00:19:19 +0800

    Make for binary with version info.
    Signed-off-by: Layne Peng <playne@vmware.com>

--- k8s-deploy/generate-ldflags.sh
+++ k8s-deploy/generate-ldflags.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env sh
 kubefateVersion="v1.0.3"
+
 gitCommit=$(git rev-parse "HEAD" 2>/dev/null);
 gitVersion=$(git describe --tags --match='v*' --abbrev=14 "${gitCommit}" 2>/dev/null)
 buildDate=$(date -u +'%Y-%m-%dT%H:%M:%SZ' 2>/dev/null)
-ldflags+="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.gitVersion=${gitVersion}'"
+git_version="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.gitVersion=${gitVersion}'"
+git_commit="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.gitCommit=${gitCommit}'"
+build_data="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.buildDate=${buildDate}'"
+kubefate_version="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.kubefateVersion=${kubefateVersion}'"
 
-ldflags+="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.gitCommit=${gitCommit}'"
-
-ldflags+="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.buildDate=${buildDate}'"
-
-ldflags+="-X 'github.com/FederatedAI/KubeFATE/k8s-deploy/pkg/service.kubefateVersion=${kubefateVersion}'"
-
-echo ${ldflags}
+echo "${git_version} ${git_commit} ${build_data} ${kubefate_version}"
