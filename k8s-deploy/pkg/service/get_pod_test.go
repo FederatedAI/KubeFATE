59e10586cee9faba7910ffbc2e8f5c226bba607a
ChenLong Ma <owlet42@126.com>
2020-08-11 11:13:28 +0800

    fix: Solve the go vet error
    Signed-off-by: ChenLong Ma <owlet42@126.com>

--- k8s-deploy/pkg/service/get_pod_test.go
+++ k8s-deploy/pkg/service/get_pod_test.go
@@ -36,42 +36,6 @@ func TestAbc(t *testing.T) {
 	fmt.Println(config.String())
 }
 
-func Test_checkClusterStatus(t *testing.T) {
-	type args struct {
-		namespace string
-		name      string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    bool
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-		{
-			name: "cluster is running",
-			args: args{
-				namespace: "fate-10000",
-				name:      "fate-10000",
-			},
-			want:    true,
-			wantErr: false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := CheckClusterStatus(tt.args.namespace, tt.args.name)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("checkClusterStatus() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("checkClusterStatus() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestGetPods(t *testing.T) {
 	type args struct {
 		name      string
