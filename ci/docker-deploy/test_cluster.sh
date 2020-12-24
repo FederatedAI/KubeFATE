fb7d6d41fcff0adb9d58bf33f66e0494afce430d
jiahaoc <jiahaoc@vmware.com>
2020-12-24 11:06:00 +0800

    restruct CI dir
    Signed-off-by: jiahaoc <jiahaoc@vmware.com>

--- ci/docker-deploy/test_cluster.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "# toy_example test"
-sudo docker exec confs-10000_python_1 bash -c "source /data/projects/python/venv/bin/activate&&/data/projects/python/venv/bin/python /data/projects/fate/python/examples/toy_example/run_toy_example.py 10000 10000 1"
-echo "# test is ok!"
