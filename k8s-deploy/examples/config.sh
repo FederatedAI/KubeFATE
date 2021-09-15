9ea3893309628002ed6384656fb010720aaacff3
ChenLong Ma <owlet42@126.com>
2021-09-15 11:12:59 +0800

    fix cluster.yaml of example
    Signed-off-by: ChenLong Ma <owlet42@126.com>

--- k8s-deploy/examples/config.sh
+++ k8s-deploy/examples/config.sh
@@ -2,22 +2,24 @@
 
 source party.config
 
-echo ${chartVersion}
-echo ${imageTAG}
+echo ${fate_chartVersion}
+echo ${fate_imageTAG}
+echo ${fate_serving_chartVersion}
+echo ${fate_serving_imageTAG}
 echo ${party_9999_IP}
 echo ${party_10000_IP}
 echo ${party_exchange_IP}
 
 # 9999 config
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-9999/cluster.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-9999/cluster-serving.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-9999/cluster-spark.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-9999/cluster-spark-pulsar.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-9999/cluster.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_serving_chartVersion}/g" ./party-9999/cluster-serving.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-9999/cluster-spark.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-9999/cluster-spark-pulsar.yaml
 
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-9999/cluster.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-9999/cluster-serving.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-9999/cluster-spark.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-9999/cluster-spark-pulsar.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-9999/cluster.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_serving_imageTAG}/g" ./party-9999/cluster-serving.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-9999/cluster-spark.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-9999/cluster-spark-pulsar.yaml
 
 
 sed -i "s/192.168.9.1/${party_9999_IP}/g" ./party-9999/cluster.yaml
@@ -37,15 +39,15 @@ sed -i "s/192.168.0.1/${party_exchange_IP}/g" ./party-9999/cluster-spark-pulsar.
 
 # 10000 config
 
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-10000/cluster.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-10000/cluster-serving.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-10000/cluster-spark.yaml
-sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-10000/cluster-spark-pulsar.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-10000/cluster.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_serving_chartVersion}/g" ./party-10000/cluster-serving.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-10000/cluster-spark.yaml
+sed -i "s/chartVersion: .*/chartVersion: ${fate_chartVersion}/g" ./party-10000/cluster-spark-pulsar.yaml
 
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-10000/cluster.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-10000/cluster-serving.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-10000/cluster-spark.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-10000/cluster-spark-pulsar.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-10000/cluster.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_serving_imageTAG}/g" ./party-10000/cluster-serving.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-10000/cluster-spark.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-10000/cluster-spark-pulsar.yaml
 
 sed -i "s/192.168.9.1/${party_9999_IP}/g" ./party-10000/cluster.yaml
 sed -i "s/192.168.9.1/${party_9999_IP}/g" ./party-10000/cluster-serving.yaml
@@ -68,8 +70,8 @@ sed -i "s/192.168.0.1/${party_exchange_IP}/g" ./party-10000/cluster-spark-pulsar
 sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-exchange/rollsite.yaml
 sed -i "s/chartVersion: .*/chartVersion: ${chartVersion}/g" ./party-exchange/trafficServer.yaml
 
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-exchange/rollsite.yaml
-sed -i "s/imageTag: .*/imageTag: ${imageTAG}/g" ./party-exchange/trafficServer.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-exchange/rollsite.yaml
+sed -i "s/imageTag: .*/imageTag: ${fate_imageTAG}/g" ./party-exchange/trafficServer.yaml
 
 sed -i "s/192.168.9.1/${party_9999_IP}/g" ./party-exchange/rollsite.yaml
 sed -i "s/192.168.9.1/${party_9999_IP}/g" ./party-exchange/trafficServer.yaml
