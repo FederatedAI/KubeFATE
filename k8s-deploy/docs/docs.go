a409b1b2139cdc6e604ef892539e2ef1c6e93fa0
ChenLong Ma <owlet42@126.com>
2022-03-20 01:12:54 +0800

    Fix getting "the server could not find the requested resource" when using `kubefate describe` (#553)
    * fix kubefate bug in k8s v1.22+
    kubefate cluster describe got "the server could not find the requested resource"
    because k8s API 'networkingv1beta1' is deprecated
    
    Signed-off-by: ChenLong Ma <owlet42@126.com>
    Signed-off-by: owlet42 <owlet42@126.com>
    
    * Add version for bug
    
    Signed-off-by: ChenLong Ma <owlet42@126.com>
    Signed-off-by: owlet42 <owlet42@126.com>
    
    * fix Deployment resources in special states are not recognized.
    When the pod is in ImagePullBackOff
    
    Signed-off-by: ChenLong Ma <owlet42@126.com>
    Signed-off-by: owlet42 <owlet42@126.com>
    
    * fix swag api error
    
    Signed-off-by: owlet42 <owlet42@126.com>
    
    * update go.mod
    
    Signed-off-by: owlet42 <owlet42@126.com>
    
    Co-authored-by: Layne Peng <appamail@hotmail.com>
    Signed-off-by: Chen Jing <jingch@vmware.com>

--- k8s-deploy/docs/docs.go
+++ k8s-deploy/docs/docs.go
@@ -4,7 +4,7 @@ package docs
 
 import "github.com/swaggo/swag"
 
-const docTemplate_swagger = `{
+const docTemplate = `{
     "schemes": {{ marshal .Schemes }},
     "swagger": "2.0",
     "info": {
@@ -1513,6 +1513,74 @@ const docTemplate_swagger = `{
                 "tags": [
                     "User"
                 ],
+                "summary": "Get user by userId",
+                "parameters": [
+                    {
+                        "type": "string",
+                        "description": "User",
+                        "name": "userId",
+                        "in": "path",
+                        "required": true
+                    },
+                    {
+                        "type": "string",
+                        "description": "Authentication header",
+                        "name": "Authorization",
+                        "in": "header",
+                        "required": true
+                    }
+                ],
+                "responses": {
+                    "200": {
+                        "description": "Success",
+                        "schema": {
+                            "allOf": [
+                                {
+                                    "$ref": "#/definitions/api.JSONResult"
+                                },
+                                {
+                                    "type": "object",
+                                    "properties": {
+                                        "data": {
+                                            "$ref": "#/definitions/modules.User"
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    "400": {
+                        "description": "Bad Request",
+                        "schema": {
+                            "$ref": "#/definitions/api.JSONERRORResult"
+                        }
+                    },
+                    "401": {
+                        "description": "Unauthorized operation",
+                        "schema": {
+                            "$ref": "#/definitions/api.JSONERRORResult"
+                        }
+                    },
+                    "500": {
+                        "description": "Internal server error",
+                        "schema": {
+                            "$ref": "#/definitions/api.JSONERRORResult"
+                        }
+                    }
+                }
+            },
+            "delete": {
+                "security": [
+                    {
+                        "ApiKeyAuth": []
+                    }
+                ],
+                "produces": [
+                    "application/json"
+                ],
+                "tags": [
+                    "User"
+                ],
                 "summary": "Delete user by userId",
                 "parameters": [
                     {
@@ -1975,8 +2043,8 @@ const docTemplate_swagger = `{
     }
 }`
 
-// SwaggerInfo_swagger holds exported Swagger Info so clients can modify it
-var SwaggerInfo_swagger = &swag.Spec{
+// SwaggerInfo holds exported Swagger Info so clients can modify it
+var SwaggerInfo = &swag.Spec{
 	Version:          "v1",
 	Host:             "",
 	BasePath:         "/v1",
@@ -1984,9 +2052,9 @@ var SwaggerInfo_swagger = &swag.Spec{
 	Title:            "KubeFATE service API",
 	Description:      "This is a KubeFATE.",
 	InfoInstanceName: "swagger",
-	SwaggerTemplate:  docTemplate_swagger,
+	SwaggerTemplate:  docTemplate,
 }
 
 func init() {
-	swag.Register(SwaggerInfo_swagger.InstanceName(), SwaggerInfo_swagger)
+	swag.Register(SwaggerInfo.InstanceName(), SwaggerInfo)
 }
